1aeb7fc34c310da5e45db9a0ceb862466e8bfb03
nohh4 <20082008sash@gmail.com>
2026-09-21 17:29:59 +0300

    завд26

---
+++ завд26лаб1.py
@@ -0,0 +1,8 @@
+days = int(input("Days: "))
+hours = int(input("Hours: "))
+minutes = int(input("Minutes: "))
+seconds = int(input("Seconds: "))
+
+total_seconds = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds
+
+print("The amounts of seconds:", total_seconds)
